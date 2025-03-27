60e90034d981e91a2e76042c1a64efaa93370584
Bao Nguyen Nguyen <baonguyen@Baos-MacBook-Pro.local>
2025-03-27 13:27:54 -0500

    test API with playwright

--- src/test/java/com/serenitydojo/playwright/toolshop/domain/User.java
+++ src/test/java/com/serenitydojo/playwright/toolshop/domain/User.java
@@ -2,10 +2,7 @@ package com.serenitydojo.playwright.toolshop.domain;
 
 import net.datafaker.Faker;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public record User(
+public record User (
         String first_name,
         String last_name,
         String address,
@@ -17,15 +14,10 @@ public record User(
         String dob,
         String password,
         String email) {
+
     public static User randomUser() {
+        // Use datafaker to fake User data
         Faker fake = new Faker();
-
-        int year = fake.number().numberBetween(1970, 2000);
-        int month = fake.number().numberBetween(1, 12);
-        int day = fake.number().numberBetween(1, 28);
-        LocalDate date = LocalDate.of(year, month, day);
-        String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
         return new User(
                 fake.name().firstName(),
                 fake.name().lastName(),
@@ -35,13 +27,13 @@ public record User(
                 fake.address().country(),
                 fake.address().postcode(),
                 fake.phoneNumber().phoneNumber(),
-                formattedDate,
+                "1900-01-01",
                 "Az123!&xyz",
                 fake.internet().emailAddress()
         );
     }
 
-    public User withPassword(String password) {
+    public Object withPassword(String password) {
         return new User(
                 first_name,
                 last_name,
@@ -53,11 +45,7 @@ public record User(
                 phone,
                 dob,
                 password,
-                email);
+                email
+        );
     }
-
-    public User withFirstName(String first_name) {
-        return new User(first_name,last_name,address,city,state,country,postcode,phone,dob,password,email);
-    }
-
 }
--- src/test/java/com/serenitydojo/playwright/toolshop/login/RegisterUserAPITest.java
+++ src/test/java/com/serenitydojo/playwright/toolshop/login/RegisterUserAPITest.java
@@ -9,89 +9,65 @@ import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.options.RequestOptions;
 import com.serenitydojo.playwright.toolshop.domain.User;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.platform.engine.support.descriptor.FileSystemSource;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @UsePlaywright
 public class RegisterUserAPITest {
-
     private APIRequestContext request;
-    private Gson gson = new Gson();
 
     @BeforeEach
     void setup(Playwright playwright) {
         request = playwright.request().newContext(
-                new APIRequest.NewContextOptions().setBaseURL("https://api.practicesoftwaretesting.com")
+                new APIRequest.NewContextOptions()
+                        .setBaseURL("https://api.practicesoftwaretesting.com")
         );
     }
-
-    @Test
-    void first_name_is_mandatory() {
-        User userWithNoName = User.randomUser().withFirstName(null);
-
-        var response = request.post("/users/register",
-                RequestOptions.create()
-                        .setHeader("Content-Type", "application/json")
-                        .setData(userWithNoName)
-        );
-
-        assertSoftly(softly -> {
-            softly.assertThat(response.status()).isEqualTo(422);
-            JsonObject responseObject = gson.fromJson(response.text(), JsonObject.class);
-            softly.assertThat(responseObject.has("first_name")).isTrue();
-            String errorMessage = responseObject.get("first_name").getAsString();
-            softly.assertThat(errorMessage).isEqualTo("The first name field is required.");
-        });
-    }
-
 
     @AfterEach
-    void tearDown() {
-        if (request != null) {
+    void teardown() {
+        if(request != null) {
             request.dispose();
         }
     }
 
     @Test
     void should_register_user() {
+        // Create a fake user to feed API
         User validUser = User.randomUser();
 
-        var response = request.post("/users/register",
-                RequestOptions.create()
-                        .setHeader("Content-Type", "application/json")
-                        .setData(validUser)
+        // Send a Post request to API
+        var response = request.post("/users/register", RequestOptions.create()
+                .setHeader("Content-Type", "application/json")
+                .setData(validUser)
         );
 
+        // Get response body to extract data
         String responseBody = response.text();
+        Gson gson = new Gson();
         User createdUser = gson.fromJson(responseBody, User.class);
 
         JsonObject responseObject = gson.fromJson(responseBody, JsonObject.class);
 
-        assertSoftly(softly -> {
+        // Assertions
+        SoftAssertions.assertSoftly(softly -> {
             softly.assertThat(response.status())
-                    .as("Registration should return 201 created status code")
+                    .as("Should return 201 created status code")
                     .isEqualTo(201);
 
             softly.assertThat(createdUser)
-                    .as("Created user should match the specified user without the password")
+                    .as("created user should match the specified user")
                     .isEqualTo(validUser.withPassword(null));
-
-            assertThat(responseObject.has("password"))
-                    .as("No password should be returned")
-                    .isFalse();
-
-            softly.assertThat(responseObject.get("id").getAsString())
-                    .as("Registered user should have an id")
-                    .isNotEmpty();
-
-            softly.assertThat(response.headers().get("content-type")).contains("application/json");
         });
     }
-
 }
